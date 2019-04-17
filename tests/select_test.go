aa05ebb168bd893be41b0fa05bbf538f697cfd67
Barry Xu 许庭新 <barry.xu@nio.com>
2019-04-17 15:27:55 +0800

    improve test cases

--- tests/select_test.go
+++ tests/select_test.go
@@ -14,6 +14,7 @@ func TestSelect1(t *testing.T) {
 	query.AndWhere("name", "!=", "barry")
 	query.OrWhere("is_employee", "=", true)
 	query.OrWhere("birthday", "=", nil)
+	query.OrWhere("name", "like", "%go%")
 	query.Orderby("birthday", true)
 	query.Orderby("name", false)
 	query.Select("address")
@@ -22,7 +23,7 @@ func TestSelect1(t *testing.T) {
 	query.Where("name", "in", []string{"edwin", "leo", "jack", "stacy"})
 	query.Where("age", "not in", []int8{16, 24, 32})
 	str := query.String()
-	expected := `SELECT name,age,sex,birthday,is_employee,address FROM users WHERE age = 15 AND name != "barry" OR is_employee = true OR birthday IS null AND name in ("edwin","leo","jack","stacy") AND age not in (16,24,32) ORDER BY birthday DESC,name ASC LIMIT 36, 10`
+	expected := `SELECT name,age,sex,birthday,is_employee,address FROM users WHERE age = 15 AND name != "barry" OR is_employee = true OR birthday IS null OR name like "%go%" AND name in ("edwin","leo","jack","stacy") AND age not in (16,24,32) ORDER BY birthday DESC,name ASC LIMIT 36, 10`
 	if str != expected {
 		t.Errorf("\nExpected:\n%s\nActually:\n%s\n", expected, str)
 	}
